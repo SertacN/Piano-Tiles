a0ee8042917dbe555dd2d4cdd7759cfd1ba4e560
Sertaç Can <82063025+SertacN@users.noreply.github.com>
2023-05-03 01:35:04 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+Piano tiles with html,css and javascript.
+
+Alsa Im added jQuery version
---
+++ with jQuery/index.js
@@ -0,0 +1,57 @@
+
+$(".piano").on("click", function(){
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+})
+
+$(document).on("keypress",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function makeSound(button){
+    switch (button) {
+        case "a":
+            let doSound = new Audio("sounds/do.mp3");
+            doSound.play();
+            break;
+        case "s":
+            let reSound = new Audio("sounds/re.mp3");
+            reSound.play();
+            break;
+        case "d":
+            let miSound = new Audio("sounds/mi.mp3");
+            miSound.play();
+            break;
+        case "f":
+            let faSound = new Audio("sounds/fa.mp3");
+            faSound.play();
+            break;
+        case "j":
+            let solSound = new Audio("sounds/sol.mp3");
+            solSound.play();
+            break;
+        case "k":
+            let laSound = new Audio("sounds/la.mp3");
+            laSound.play();
+            break;
+        case "l":
+            let siSound = new Audio("sounds/si.mp3");
+            siSound.play();
+            break;
+        
+        default:
+            console.log("Wrong Key");
+            break;
+    }
+}
+
+function buttonAnimation(currentKey){
+   let activeButton = $("." + currentKey);
+   activeButton.addClass("pressed");
+
+   setTimeout(function(){
+    activeButton.removeClass("pressed");
+   }, 100);
+}
+
